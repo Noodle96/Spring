fe0a72acad03a372322110bd38365d7737db430d
Noodle96 <jorgealfredomarino2@gmail.com>
2025-11-13 11:31:50 -0500

    Add user list endpoint and corresponding HTML template
    - Implemented a new endpoint in UserController to list users.
    - Created a new Thymeleaf template for displaying the user list.
    - Updated User model to support email field in user creation.

--- springboot-web/src/main/java/com/russell/curso/springboot/webapp/springboot_web/controllers/UserController.java
+++ springboot-web/src/main/java/com/russell/curso/springboot/webapp/springboot_web/controllers/UserController.java
@@ -1,29 +1,39 @@
 package com.russell.curso.springboot.webapp.springboot_web.controllers;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.russell.curso.springboot.webapp.springboot_web.models.User;
-
+import org.springframework.web.bind.annotation.RequestParam;
 
 /**
- * UserController is a Spring MVC controller that handles HTTP requests related to user details.
+ * UserController is a Spring MVC controller that handles HTTP requests related
+ * to user details.
  * El controlador interactua con los datos y la vista.
  * 
  * <p>
- * The class is annotated with {@link org.springframework.stereotype.Controller}, which marks it as a web controller
+ * The class is annotated with
+ * {@link org.springframework.stereotype.Controller}, which marks it as a web
+ * controller
  * capable of handling web requests in a Spring Boot application.
  * </p>
  * 
  * <p>
- * The {@code details} method is mapped to the "/details" URL using the {@link org.springframework.web.bind.annotation.GetMapping} annotation.
- * This means that when a GET request is made to "/details", this method will be invoked.
+ * The {@code details} method is mapped to the "/details" URL using the
+ * {@link org.springframework.web.bind.annotation.GetMapping} annotation.
+ * This means that when a GET request is made to "/details", this method will be
+ * invoked.
  * </p>
  * 
  * <p>
- * The method adds user-related attributes ("title", "name", "lastname") to the {@link org.springframework.ui.Model} object,
+ * The method adds user-related attributes ("title", "name", "lastname") to the
+ * {@link org.springframework.ui.Model} object,
  * which makes them available to the view named "details".
  * </p>
  */
@@ -31,14 +41,26 @@ import com.russell.curso.springboot.webapp.springboot_web.models.User;
 public class UserController {
     // http://localhost:8080/details
     // @GetMapping("/details")
-    @RequestMapping(path="/details", method = RequestMethod.GET)
+    @RequestMapping(path = "/details", method = RequestMethod.GET)
     public String details(Model model) {
         User user = new User("Russell", "Tito");
         model.addAttribute("title", "User Details");
         // model.addAttribute("name", "Russell");
         // model.addAttribute("lastname", "Mendoza");
         model.addAttribute("user", user);
-        return "details"; 
+        return "details";
     }
 
+    @RequestMapping(path = "/list", method = RequestMethod.GET)
+    public String list(ModelMap model) {
+        List<User> users = new ArrayList<>();
+        users.add(new User("Russell", "Mendoza"));
+        users.add(new User("John", "Doe", "john.doe@example.com"));
+        users.add(new User("Jane", "Smith", "jane.smith@example.com"));
+        users.add(new User("Alice", "Johnson"));
+
+        model.addAttribute("title", "Listado de Usuarios");
+        model.addAttribute("users", users);
+        return "list";
+    }
 }

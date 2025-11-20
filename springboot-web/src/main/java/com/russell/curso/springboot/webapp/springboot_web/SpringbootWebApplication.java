4f6116af6b0828e353bd0def38407b2af54abf39
Noodle96 <jorgealfredomarino2@gmail.com>
2025-11-20 17:34:39 -0500

    Add values.properties for card configuration and remove redundant properties from application.properties

--- springboot-web/src/main/java/com/russell/curso/springboot/webapp/springboot_web/SpringbootWebApplication.java
+++ springboot-web/src/main/java/com/russell/curso/springboot/webapp/springboot_web/SpringbootWebApplication.java
@@ -2,8 +2,14 @@ package com.russell.curso.springboot.webapp.springboot_web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 
 @SpringBootApplication
+@PropertySources({
+		@PropertySource(value = "classpath:values.properties", encoding = "UTF-8"),
+// @PropertySource("classpath:values2.properties")
+})
 public class SpringbootWebApplication {
 
 	public static void main(String[] args) {
